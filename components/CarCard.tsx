d9155e6aa155dbc99439e8aae8a778971ddb5a3b
Farhat Mahi <farhatmahi007@gmail.com>
2023-09-30 10:18:34 +0600

    FINISHED

---
+++ components/CarCard.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { CarProps } from "@/types";
+import Image from "next/image";
+import { CarDetails, CustomButton } from ".";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
+import { useState } from "react";
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  const [isOpen, setIsOpen] = useState(false);
+  const carRent = calculateCarRent(city_mpg, year);
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p className="car-card__price">
+        <span className="car-card__price-dollar">$</span>
+        {carRent}
+        <span className="car-card__price-day">/day</span>
+      </p>
+
+      <div className="relative h-64 lg:h-40 w-full my-3 object-contain">
+        <Image src={generateCarImageUrl(car)} alt="Car model" fill priority />
+      </div>
+
+      <div className="relative w-full mt-2">
+        <div className="flex group-hover:invisible justify-between text-gray-400">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="Steering Wheel"
+            ></Image>
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="Tire"></Image>
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="Gas"></Image>
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+      </div>
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
+    </div>
+  );
+};
+
+export default CarCard;
